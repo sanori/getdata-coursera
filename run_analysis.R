eb2e4b8f7c9fb1c02597883d3483c1f45492ea7c
Joo-Won Jung <sanori@gmail.com>
2015-08-22 15:26:08 +0900

    refactor dataset generation and add summary generation routine

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,6 @@
 
-
 dataDir <- "./UCI HAR Dataset"
 
-# Load dataset and drop unnecessary columns
 dataset <- local({
     # extract nessesary columns
     features <- read.table(paste(dataDir, "features.txt",
@@ -10,32 +8,35 @@ dataset <- local({
     necFeatures <- features[grepl("-(mean|std)\\(",features$V2),]
     columns <- necFeatures$V1
     
-    datasetTest <- read.table(paste(dataDir, "test/X_test.txt",
+    # Load features(predictors) and drop unnecessary variables
+    datasetTest <- read.table(paste(dataDir, "test", "X_test.txt",
                                     sep="/"))[,columns]
-    datasetTrain <- read.table(paste(dataDir, "train/X_train.txt",
+    datasetTrain <- read.table(paste(dataDir, "train", "X_train.txt",
                                      sep="/"))[,columns]
     d <- rbind(datasetTrain, datasetTest)
     names(d) <- necFeatures$V2
+    
+    # Load outcome variables and attach to the dataset
+    activityTest <- read.table(paste(dataDir, "test", "Y_test.txt", sep="/"))
+    activityTrain <- read.table(paste(dataDir, "train", "Y_train.txt", sep="/"))
+    d$activity.id <- rbind(activityTrain, activityTest)$V1
+    
+    # Load subject variable and attach to the dataset
+    subjectTest <- read.table(paste(dataDir, "test", "subject_test.txt", sep="/"))
+    subjectTrain <- read.table(paste(dataDir, "train", "subject_train.txt", sep="/"))
+    d$subjectId <- rbind(subjectTrain, subjectTest)$V1
+    
     d
 })
 
-labeledActivity <- local({
-    activityTest <- read.table(paste(dataDir, "test/Y_test.txt", sep="/"))
-    activityTrain <- read.table(paste(dataDir, "train/Y_train.txt", sep="/"))
-    activity <- rbind(activityTrain, activityTest)
-    names(activity) <- "activity.id"
-    activityLabels <- read.table(paste(dataDir, "activity_labels.txt", sep="/"),
+# join with descriptive values
+activityLabels <- read.table(paste(dataDir, "activity_labels.txt", sep="/"),
                                  col.names=c("activity.id", "activity.name"))
-    merge(activity, activityLabels)
-})
+dataset <- merge(dataset, activityLabels, all.x = TRUE)
+dataset$activity.id <- NULL
 
-subject <- local({
-    subjectTest <- read.table(paste(dataDir, "test/subject_test.txt", sep="/"))
-    subjectTrain <- read.table(paste(dataDir, "train/subject_train.txt", sep="/"))
-    rbind(subjectTrain, subjectTest)
-})
+# make summay data
+summary <- aggregate(. ~ subjectId + activity.name, data=dataset, mean)
+summary <- summary[order(summary$subjectId, summary$activity),]
 
-names(subject) <- "subjectId"
-dataset$activity <- labeledActivity$activity.name
-dataset$subjectId <- subject
-
+write.table(summary, "summary.txt", row.names = FALSE)
